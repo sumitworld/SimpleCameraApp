bdec6420a36b4ff3f6980e8fc581fa2a73ffaac3
sumitworld <sumitsam465@gmail.com>
2024-01-22 09:40:47 -0700

    Initial Commit

---
+++ SimpleCameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCameraApp
+//
+//  Created by Vivek Parmar on 2024-01-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
